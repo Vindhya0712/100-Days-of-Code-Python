37156cfb0aec7dc2e3005a3ba3f54768fa239f41
Vindhya0712 <svh.aiml2026@gmail.com>
2026-07-06 23:04:17 +0530

    Created Ball class and got the ball to bounce off when reaching edges or at collision with paddles

--- Day22-Pong-Game/main.py
+++ Day22-Pong-Game/main.py
@@ -1,5 +1,7 @@
 from turtle import Screen
 from paddle import Paddle
+from ball import Ball
+import time
 
 screen = Screen()
 screen.bgcolor('black')
@@ -9,6 +11,7 @@ screen.tracer(0)
 
 right_paddle = Paddle((350, 0))
 left_paddle = Paddle((-350, 0))
+ball = Ball()
 screen.update()
 
 screen.listen()
@@ -20,5 +23,22 @@ screen.onkey(key='s', fun=left_paddle.move_down)
 game_is_on = True
 while game_is_on:
     screen.update()
+    time.sleep(0.1)
+    ball.move()
+    # Detect collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        # Bounce
+        ball.bounce_y()
+
+    # Detect collision with both paddle
+    if ball.distance(right_paddle) < 50 and ball.xcor() > 320:
+        print('Made contact')
+        ball.bounce_x()
+
+    elif ball.distance(left_paddle) < 50 and ball.xcor() < -320:
+        print('Made contact')
+        ball.bounce_x()
+
+
 
 screen.exitonclick()
